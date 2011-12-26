4babc8cee42deabd12f7ee74388ebd9a3e23939f
Ashish Dubey <ashish.dubey91@gmail.com>
2011-12-27 00:35:58 +0530

    Tweet works on saving page

--- SpecialTweetiki.php
+++ SpecialTweetiki.php
@@ -1,21 +1,30 @@
 <?php
-class ashish extends SpecialPage {
-        function __construct() {
-                parent::__construct( 'ashish' );
-        }
- 
-        function execute( $par ) {
-                global $wgRequest, $wgOut;
- 
-                $this->setHeaders();
- 
-                # Get request data from, e.g.
-         $param = $wgRequest->getText('param');
- 
-                # Do stuff
-         # ...
-         $output = 'Hello world!';
-                $wgOut->addWikiText( $output );
-        }
+include dirname(__FILE__).'/includes/TwitterOAuth/twitteroauth.php';
+class SpecialTweetiki extends SpecialPage
+{
+    function __construct()
+    {
+        parent::__construct( 'Tweetiki' );
+    }
+
+    function execute( $par )
+    {
+        global $wgRequest, $wgOut;
+        $wikiurl = 'http://localhost/mediawiki/';
+        $this->setHeaders();
+        $oauth_token = $wgRequest->getVal('oauth_token');
+        $oauth_verifier = $wgRequest->getVal('oauth_verifier');
+      	$connection=new TwitterOAuth('IPs4LAerAqSybJB9uOJ0A', 
+                    'pQngKocjGAp2FnTMly4GEMr8wc0Khu0ko9QhlEQSHI',
+                    $oauth_token, $_SESSION['tw_oauth_token_secret']);
+        $token = $_SESSION['tw_oauth_token'];
+        $token_credentials = $connection->getAccessToken($oauth_verifier);
+        $url = $wikiurl . 'index.php?title=' . $wgRequest->getVal('urltitle');
+        $curl = curl_init('https://www.googleapis.com/urlshortener/v1/url');
+        $text = 'I just edited a wiki page. Be bold, and edit. ' . $url;
+        $result = $connection->post('statuses/update',array('status' => $text));  
+        $wgOut->setTitle('Tweetiki');
+        $wgOut->addWikiText('Done');
+    }
 }
 ?>
--- Tweetiki.ext.php
+++ Tweetiki.ext.php
@@ -2,37 +2,38 @@
 include dirname(__FILE__).'/includes/TwitterOAuth/twitteroauth.php';
 class Tweetiki
 {
-    static function getLoginLink()
+    static function getLoginLink($params)
     {
-        $wiki_url = '';
-        $connection=new TwitterOAuth("IPs4LAerAqSybJB9uOJ0A","pQngKocjGAp2FnTMly4GEMr8wc0Khu0ko9QhlEQSHI");
-        $temporary_credentials=$connection->getRequestToken($wiki_url.'Special:TwitterCallback');
-        $_SESSION['tw_oauth_token']=$token=$temporary_credentials['oauth_token'];
-        $_SESSION['tw_oauth_token_secret']=$temporary_credentials['oauth_token_secret'];
-        $redirect_url=$connection->getAuthorizeURL($temporary_credentials);
+        $wiki_url = 'http://localhost/mediawiki/';
+        $connection = new TwitterOAuth('IPs4LAerAqSybJB9uOJ0A', 
+                      'pQngKocjGAp2FnTMly4GEMr8wc0Khu0ko9QhlEQSHI');
+        $temporary_credentials = $connection->getRequestToken($wiki_url .
+                                 'index.php?title=Special:Tweetiki' . '&' .
+                                 $params);
+        $_SESSION['tw_oauth_token'] = $token=$temporary_credentials['oauth_token'];
+        $_SESSION['tw_oauth_token_secret'] = $temporary_credentials['oauth_token_secret'];
+        $redirect_url = $connection->getAuthorizeURL($temporary_credentials);
         return $redirect_url;
     }    
-    static function showTweetCheck( &$editpage, &$checks, &$tabindex )
+    static function showTweetCheck(&$editpage, &$checks, &$tabindex)
     {
         global $wgOut;
         $attribs = array(
                           'tabindex'  => ++$tabindex,
                           'accesskey' => 'accesskey-sendtotwitter',
                           'id'        => 'TweetThis');
-        $checkmarkup = Xml::check( 'TweetThis', true, $attribs );
+        $checkmarkup = Xml::check('TweetThis', true, $attribs);
         $label = '&nbsp;<label for="TweetThis" title="' .
                   'Check This for Tweet'.'">' .
                   'Tweet This</label>';
         $checks['tweetiki'] = $checkmarkup . $label;
         $wgOut->includeJQuery();
-        $login_link = Tweetiki::getLoginLink();
+        $params = 'urltitle=' . $editpage->mTitle->mUrlform;
+        $login_link = Tweetiki::getLoginLink($params);
         $script = '$("#wpSave").click(function(){window.open("' . $login_link
                    . '");});';
-        $wgOut->addScript( Html::inlineScript( $script ) );
+        $wgOut->addScript(Html::inlineScript($script));
         return true;
     }
 }
-        
-
-
 
